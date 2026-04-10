1bde126c59ff4a43a1e8173d0b11d0c9e5d132f6
Elkin <elkin1506ml@gmail.com>
2026-04-10 10:04:30 -0500

    Navegacion Login -> Register

--- app/src/main/java/com/elkinmendoza/exploraapp/MainActivity.kt
+++ app/src/main/java/com/elkinmendoza/exploraapp/MainActivity.kt
@@ -28,10 +28,25 @@ class MainActivity : ComponentActivity() {
                 modifier = Modifier.fillMaxSize()
             ) {
                 composable (route = "login"){
-                    LoginScreen(onLoginSuccess = {}, onNavigateToRegister = {})
+                    LoginScreen(
+                        onLoginSuccess = {
+                        },
+                        onNavigateToRegister = {
+                            myNavController.navigate("register")
+                        }
+                    )
                 }
                 composable (route = "register") {
-                    RegisterScreen(onRegisterSuccess = {}, onNavigateToLogin = {})
+                    RegisterScreen(
+                        onRegisterSuccess = {
+
+                        },
+                        onNavigateToLogin = {
+                        },
+                        onBackClick ={
+                            myNavController.popBackStack()
+                        }
+                    )
                 }
             }
         }
